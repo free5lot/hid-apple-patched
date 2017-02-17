bf717a84aaff5ee2f0addfe0cbadaf8257f4b01c
Jan Meznik <jan@meznik.dk>
2017-02-17 09:31:12 +0100

    Modified build.sh to also build on Fedora.
    build.sh should now be able to build on Fedora as well.
    Tested on Fedora 25.

--- build.sh
+++ build.sh
@@ -6,7 +6,19 @@
 # https://github.com/free5lot/
 ###################################################
 
-LINUX_HEADER_DIR="${LINUX_HEADER_DIR:-/usr/src/linux-headers-$(uname -r)}"
+if [ -f /etc/fedora-release ] ; then
+	DIST='Fedora'
+	LINUX_HEADER_DIR="/lib/modules/$(uname -r)/build"
+
+elif [ -f /etc/debian_version ] ; then
+	DIST="Debian"
+	LINUX_HEADER_DIR="${LINUX_HEADER_DIR:-/usr/src/linux-headers-$(uname -r)}"
+
+else
+	echo "Could not determine Linux distribution!"
+	exit 1
+fi
+
 
 # Check if $LINUX_HEADER_DIR exists
 if [ ! -d "$LINUX_HEADER_DIR" ]; then
@@ -14,11 +26,16 @@ if [ ! -d "$LINUX_HEADER_DIR" ]; then
 	echo "Error: Linux headers directory was not found."
 	echo "Searched for: $LINUX_HEADER_DIR"
 	echo ""
-	echo "Try to install it to solve this problem, e.g:"
+	echo "Try to install it to solve this problem,     "
+	echo "On Ubuntu:"
 	echo "sudo apt-get install linux-headers-`uname -r`"
+	echo ""
+	echo "On Fedora:"
+	echo "dnf install kernel-devel"
 	echo "============================================="
-	exit;
+	exit 1;
 fi
+
 
 # Make hid-apple.ko module
 echo "============================================="
